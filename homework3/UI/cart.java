27c09502033e6048844daef29d94a7470a63af6c
FeryChuang <119273274+FeryChuang@users.noreply.github.com>
2023-05-31 17:54:56 +0800

    V2

--- homework3/UI/cart.java
+++ homework3/UI/cart.java
@@ -7,10 +7,23 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import homework3.prog.product;
 
 public class cart extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField id;
+	private JTextField cash;
+	private
 
 	/**
 	 * Launch the application.
@@ -50,23 +63,88 @@ public class cart extends JFrame {
 		lblNewLabel_2.setBounds(132, 10, 150, 45);
 		panel.add(lblNewLabel_2);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(10, 85, 207, 240);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(24, 34, 46, 15);
-		panel_1.add(lblNewLabel);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(10, 335, 414, 316);
-		contentPane.add(panel_2);
-		panel_2.setLayout(null);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 85, 414, 566);
+		contentPane.add(tabbedPane);
 		
 		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(217, 85, 207, 240);
-		contentPane.add(panel_3);
+		tabbedPane.addTab("販售", null, panel_3, null);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_5 = new JLabel("會員ID");
+		lblNewLabel_5.setBounds(32, 40, 80, 15);
+		panel_3.add(lblNewLabel_5);
+		
+		id = new JTextField();
+		id.setBounds(122, 34, 80, 20);
+		panel_3.add(id);
+		id.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("乾飼料 $1000");
+		lblNewLabel.setBounds(32, 65, 80, 20);
+		panel_3.add(lblNewLabel);
+		
+		JSpinner dry = new JSpinner();
+		dry.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
+		dry.setBounds(122, 65, 80, 20);
+		panel_3.add(dry);
+		
+		JLabel lblNewLabel_1 = new JLabel("罐頭 $60");
+		lblNewLabel_1.setBounds(32, 95, 80, 20);
+		panel_3.add(lblNewLabel_1);
+		
+		JSpinner can = new JSpinner();
+		can.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
+		can.setBounds(122, 95, 80, 20);
+		panel_3.add(can);
+		
+		JLabel lblNewLabel_3 = new JLabel("主食餐包 $45");
+		lblNewLabel_3.setBounds(32, 125, 80, 20);
+		panel_3.add(lblNewLabel_3);
+		
+		JSpinner bag = new JSpinner();
+		bag.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
+		bag.setBounds(122, 125, 80, 20);
+		panel_3.add(bag);
+		
+		JCheckBox member = new JCheckBox("會員價9折");
+		member.setBounds(32, 163, 120, 20);
+		panel_3.add(member);
+		
+		JLabel lblNewLabel_4 = new JLabel("付款方式");
+		lblNewLabel_4.setBounds(32, 198, 80, 20);
+		panel_3.add(lblNewLabel_4);
+		
+		JRadioButton payCash = new JRadioButton("現金實付");
+		payCash.setBounds(32, 224, 80, 20);
+		panel_3.add(payCash);
+		
+		JRadioButton payCredit = new JRadioButton("信用卡 手續費3%");
+		payCredit.setBounds(32, 253, 120, 20);
+		panel_3.add(payCredit);
+				
+		cash = new JTextField();
+		cash.setBounds(122, 224, 80, 20);
+		panel_3.add(cash);
+		cash.setColumns(10);
+		
+		JButton btnNewButton = new JButton("小結");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				product p1=new product(id.getText()+)
+			}
+		});
+		btnNewButton.setBounds(32, 292, 87, 23);
+		panel_3.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("付款");
+		btnNewButton_2.setBounds(129, 292, 87, 23);
+		panel_3.add(btnNewButton_2);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("訂單", null, panel_1, null);
+		panel_1.setLayout(null);
 	}
-
 }
--- homework3/prog/product.java
+++ homework3/prog/product.java
@@ -1,5 +1,110 @@
 package homework3.prog;
 
 public class product {
+	private String orderNumber;
+	private String id;
+	private boolean member;
+	private int dry;
+	private int can;
+	private int bag;
+	private int sum;
+	private boolean payment;
+	static int index = 0;
+	
+	
+	public product(String orderNumber, String id, boolean member, int dry, int can, int bag, boolean payment) {
+		super();
+			this.orderNumber = orderNumber;
+			this.id = id;
+			this.dry = dry;
+			this.can = can;
+			this.bag = bag;
+			index++;
+		if(member==true && payment==true) {
+			sum=(int)((dry*1380+can*60+bag*45)*0.9);
+		}else if (member==false && payment==true){
+			sum=(int)(dry*1380+can*60+bag*45);
+		}else if(member==true && payment==false) {
+			sum=(int)((dry*1380+can*60+bag*45)*0.9*1.03);
+		}else if(member==false && payment==false) {
+			sum=(int)((dry*1380+can*60+bag*45)*1.03);
+		}
+		
+	}
+
+	
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public boolean isMember() {
+		return member;
+	}
+
+
+	public void setMember(boolean member) {
+		this.member = member;
+	}
+
+
+	public int getDry() {
+		return dry;
+	}
+
+
+	public void setDry(int dry) {
+		this.dry = dry;
+	}
+
+
+	public int getCan() {
+		return can;
+	}
+
+
+	public void setCan(int can) {
+		this.can = can;
+	}
+
+
+	public int getBag() {
+		return bag;
+	}
+
+
+	public void setBag(int bag) {
+		this.bag = bag;
+	}
+
+
+	public boolean isPayment() {
+		return payment;
+	}
+
+
+	public void setPayment(boolean payment) {
+		this.payment = payment;
+	}
+
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+
+	public int getSum() {
+		return sum;
+	}
+	
+	
+	
+	
+	
 
 }
