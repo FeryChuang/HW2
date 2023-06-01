c53225ee20660743c19a1635045b04409c438339
FeryChuang <119273274+FeryChuang@users.noreply.github.com>
2023-06-01 17:26:32 +0800

    V4

--- homework3/UI/cart.java
+++ homework3/UI/cart.java
@@ -5,8 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.ScrollPane;
+
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
@@ -16,17 +21,26 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.print.PrinterException;
+
 import homework3.prog.product;
 import javax.swing.ButtonGroup;
 import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class cart extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField id;
 	private JTextField cash;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private product p1=null;
+	private JTextField id;
+	DefaultTableModel model;
+	private JTable table;
+	
 
 	/**
 	 * Launch the application.
@@ -67,103 +81,235 @@ public class cart extends JFrame {
 		panel.add(lblNewLabel_2);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 85, 414, 566);
+		tabbedPane.setBounds(10, 229, 414, 383);
 		contentPane.add(tabbedPane);
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("販售", null, panel_3, null);
 		panel_3.setLayout(null);
-		
-		JLabel lblNewLabel_5 = new JLabel("會員ID");
-		lblNewLabel_5.setBounds(32, 40, 80, 15);
-		panel_3.add(lblNewLabel_5);
-		
-		id = new JTextField();
-		id.setBounds(122, 34, 80, 20);
-		panel_3.add(id);
-		id.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("乾飼料 $1000");
-		lblNewLabel.setBounds(32, 65, 80, 20);
-		panel_3.add(lblNewLabel);
-		
-		JSpinner dry = new JSpinner();
-		dry.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
-		dry.setBounds(122, 65, 80, 20);
-		panel_3.add(dry);
-		
-		JLabel lblNewLabel_1 = new JLabel("罐頭 $50");
-		lblNewLabel_1.setBounds(32, 95, 80, 20);
-		panel_3.add(lblNewLabel_1);
-		
-		JSpinner can = new JSpinner();
-		can.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
-		can.setBounds(122, 95, 80, 20);
-		panel_3.add(can);
-		
-		JLabel lblNewLabel_3 = new JLabel("主食餐包 $40");
-		lblNewLabel_3.setBounds(32, 125, 80, 20);
-		panel_3.add(lblNewLabel_3);
-		
-		JSpinner bag = new JSpinner();
-		bag.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
-		bag.setBounds(122, 125, 80, 20);
-		panel_3.add(bag);
-		
-		JCheckBox member = new JCheckBox("會員價9折");
-		member.setBounds(32, 163, 120, 20);
-		panel_3.add(member);
-		
-		JLabel lblNewLabel_4 = new JLabel("付款方式");
-		lblNewLabel_4.setBounds(32, 198, 80, 20);
-		panel_3.add(lblNewLabel_4);
-		
-		JRadioButton payCash = new JRadioButton("現金實付");
-		buttonGroup.add(payCash);
-		payCash.setBounds(32, 224, 80, 20);
-		panel_3.add(payCash);
-		
-		JRadioButton payCredit = new JRadioButton("信用卡 手續費3%");
-		buttonGroup.add(payCredit);
-		payCredit.setBounds(32, 253, 120, 20);
-		panel_3.add(payCredit);
 				
-		cash = new JTextField();
-		cash.setBounds(122, 224, 80, 20);
-		panel_3.add(cash);
-		cash.setColumns(10);
 		
-		JTextArea output = new JTextArea();
-		output.setBounds(32, 329, 347, 180);
-		panel_3.add(output);
-		
-		JButton btnNewButton = new JButton("小結");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				boolean PM=true;
-				if(payCash.isSelected()) {PM=true;}					
-				if(payCredit.isSelected()) {PM=false;}
-				p1=new product(id.getText(),member.isSelected(),((int)dry.getValue()),((int)can.getValue()),((int)bag.getValue()),PM);
-				output.setText(p1.output());
-			}
-		});
-		btnNewButton.setBounds(32, 292, 87, 23);
-		panel_3.add(btnNewButton);
-		
-		JButton btnNewButton_2 = new JButton("付款");
-		btnNewButton_2.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
-		btnNewButton_2.setBounds(129, 292, 87, 23);
-		panel_3.add(btnNewButton_2);
+		JLabel lblNewLabel_3 = new JLabel("現金實付");
+		lblNewLabel_3.setBounds(46, 292, 54, 20);
+		panel_3.add(lblNewLabel_3);
 		
 		
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("訂單", null, panel_1, null);
 		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_5 = new JLabel("會員ID");
+		lblNewLabel_5.setBounds(28, 119, 80, 15);
+		contentPane.add(lblNewLabel_5);
+		
+		id = new JTextField();
+		id.setColumns(10);
+		id.setBounds(118, 113, 80, 20);
+		contentPane.add(id);
+		
+		JLabel lblNewLabel = new JLabel("乾飼料 $1000");
+		lblNewLabel.setBounds(28, 144, 80, 20);
+		contentPane.add(lblNewLabel);
+		
+		JSpinner dry = new JSpinner();
+		dry.setBounds(118, 144, 80, 20);
+		contentPane.add(dry);
+		
+		JLabel lblNewLabel_1 = new JLabel("罐頭 $50");
+		lblNewLabel_1.setBounds(28, 174, 80, 20);
+		contentPane.add(lblNewLabel_1);
+		
+		JSpinner can = new JSpinner();
+		can.setBounds(118, 174, 80, 20);
+		contentPane.add(can);
+		
+		JCheckBox member = new JCheckBox("會員價9折");
+		member.setBounds(246, 113, 120, 20);
+		contentPane.add(member);
+		
+		JLabel lblNewLabel_4 = new JLabel("付款方式");
+		lblNewLabel_4.setBounds(246, 148, 80, 20);
+		contentPane.add(lblNewLabel_4);
+		
+		JRadioButton payCash = new JRadioButton("現金實付");
+		payCash.setSelected(true);
+		buttonGroup.add(payCash);
+		payCash.setBounds(246, 174, 80, 20);
+		contentPane.add(payCash);
+		
+		JRadioButton payCredit = new JRadioButton("信用卡 手續費3%");
+		buttonGroup.add(payCredit);
+		payCredit.setBounds(246, 203, 120, 20);
+		contentPane.add(payCredit);
+		
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 80, 389, 261);
+		panel_1.add(scrollPane_1);
+		
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i=table.getSelectedRow();
+				id.setText(model.getValueAt(i,0).toString());
+				dry.setValue(model.getValueAt(i, 1));
+				can.setValue(model.getValueAt(i, 2));
+			}
+		});
+		scrollPane_1.setViewportView(table);
+		model =new DefaultTableModel();
+		Object[] column= {"ID","乾飼料","罐頭","總金額"};
+		Object[] row=new Object[4];
+		model.setColumnIdentifiers(column);
+		table.setModel(model);
+				
+		
+		cash = new JTextField();
+		cash.setBounds(41, 315, 80, 20);
+		panel_3.add(cash);
+		cash.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 45, 389, 237);
+		panel_3.add(scrollPane);
+		
+		JTextArea output = new JTextArea();
+		output.setBounds(12, 45, 387, 235);
+		panel_3.add(output);
+		
+		JButton btnNewButton = new JButton("計算");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(id.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "請輸入會員名稱");
+				}else {
+				boolean PM=true;
+				if(payCash.isSelected()) {PM=true;}					
+				if(payCredit.isSelected()) {PM=false;}
+				p1=new product(id.getText(),member.isSelected(),((int)dry.getValue()),((int)can.getValue()),PM);
+				output.setText(p1.output());
+				
+				
+					
+			}}
+		});
+		btnNewButton.setBounds(298, 10, 80, 25);
+		panel_3.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("現金結帳");
+		btnNewButton_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (cash.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "請輸入現金");
+				}else {
+				int CO=Integer.parseInt(cash.getText());
+				output.setText(p1.output()+"\n\t\t找零"+"\n\t\t\t"+(CO-p1.getSum()));
+				row[0]=id.getText();
+				row[1]=dry.getValue();
+				row[2]=can.getValue();
+				row[3]=p1.getSum();
+				model.addRow(row);
+				
+				id.setText("");
+				dry.setValue(0);
+				can.setValue(0);
+				JOptionPane.showMessageDialog(null, "訂購成功!");
+			}}
+		});
+		btnNewButton_2.setBounds(131, 313, 100, 25);
+		panel_3.add(btnNewButton_2);
+		
+		JButton btnNewButton_1 = new JButton("信用卡結帳");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				row[0]=id.getText();
+				row[1]=dry.getValue();
+				row[2]=can.getValue();
+				row[3]=p1.getSum();
+				model.addRow(row);
+				
+				id.setText("");
+				dry.setValue(0);
+				can.setValue(0);
+				JOptionPane.showMessageDialog(null, "訂購成功!");
+			}
+		});
+		btnNewButton_1.setBounds(241, 313, 100, 25);
+		panel_3.add(btnNewButton_1);
+		
+		JButton btnNewButton_6 = new JButton("列印明細");
+		btnNewButton_6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+				output.print();
+				}catch(PrinterException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnNewButton_6.setBounds(301, 358, 100, 25);
+		panel_3.add(btnNewButton_6);
+		
+		
+		
+		JButton btnNewButton_3 = new JButton("修改訂單");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(id.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "請輸入會員名稱");
+				}else{
+					boolean PM=true;
+					if(payCash.isSelected()) {PM=true;}					
+					if(payCredit.isSelected()) {PM=false;}
+					p1=new product(id.getText(),member.isSelected(),((int)dry.getValue()),((int)can.getValue()),PM);
+									
+					int i=table.getSelectedRow();
+					if(i>=0) {
+						model.setValueAt(id.getText(), i, 0);
+						model.setValueAt(dry.getValue(), i, 1);
+						model.setValueAt(can.getValue(), i, 2);
+						model.setValueAt(p1.getSum(), i, 3);
+					}
+				}
+			}
+		});
+		btnNewButton_3.setBounds(179, 27, 100, 25);
+		panel_1.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("刪除訂單");
+		btnNewButton_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i=table.getSelectedRow();
+				if(i>=0) {
+					model.removeRow(i);
+					JOptionPane.showMessageDialog(null, "已刪除!");
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "請選擇一筆訂單");
+				}
+			}
+		});
+		btnNewButton_4.setBounds(301, 27, 100, 25);
+		panel_1.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("離開");
+		btnNewButton_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton_5.setBounds(337, 628, 80, 25);
+		contentPane.add(btnNewButton_5);
+		
+		
+		
 	}
 }
--- homework3/prog/product.java
+++ homework3/prog/product.java
@@ -5,27 +5,27 @@ public class product {
 	private boolean member;
 	private int dry;
 	private int can;
-	private int bag;
 	private int sum;
 	private boolean payment;
 	
 	
 	
-	public product(String id, boolean member, int dry, int can, int bag, boolean payment) {
+	public product(String id, boolean member, int dry, int can, boolean payment) {
 		super();
 			this.id = id;
 			this.dry = dry;
 			this.can = can;
-			this.bag = bag;
-			
+			this.member=member;
+			this.payment=payment;
+						
 		if(member==true && payment==true) {
-			sum=(int)((dry*1000+can*50+bag*40)*0.9);
+			sum=(int)((dry*1000+can*50)*0.9);
 		}else if (member==false && payment==true){
-			sum=(int)(dry*1000+can*50+bag*40);
+			sum=(int)(dry*1000+can*50);
 		}else if(member==true && payment==false) {
-			sum=(int)((dry*1000+can*50+bag*40)*0.9*1.03);
+			sum=(int)((dry*1000+can*50)*0.9*1.03);
 		}else if(member==false && payment==false) {
-			sum=(int)((dry*1000+can*50+bag*40)*1.03);
+			sum=(int)((dry*1000+can*50)*1.03);
 		}
 		
 	}
@@ -50,6 +50,13 @@ public class product {
 		this.member = member;
 	}
 
+	public String showMember() {
+		if (member==true) {
+			return "會員價9折";
+		}else{
+			return "原價";
+		}
+	}
 
 	public int getDry() {
 		return dry;
@@ -76,19 +83,6 @@ public class product {
 		return this.can*50;
 	}
 	
-	public int getBag() {
-		return bag;
-	}
-
-
-	public void setBag(int bag) {
-		this.bag = bag;
-	}
-
-	public int BS() {
-		return this.bag*40;
-	}
-
 	public boolean isPayment() {
 		return payment;
 	}
@@ -98,21 +92,27 @@ public class product {
 		this.payment = payment;
 	}
 
-
+	public String showPayment() {
+		if (member==true) {
+			return "現金價";
+		}else{
+			return "外加刷卡手續費3%";
+		}
+	}
+	
 	public int getSum() {
 		return sum;
 	}
 	
 	public String output(){
 		return 
-				"收到您的訂單!\t\t\t\t^ↀᴥↀ^"+
-				"\n========================================================"+
+				"收到您的訂單!\t\t\t^ↀᴥↀ^"+
+				"\n============================================"+
 				"\n品項\t單價\t數量\t小計"+
-				"\n--------------------------------------------------------------------------------------------------"+
+				"\n--------------------------------------------------------------------------------"+
 				"\n乾飼料\t$1380\t"+getDry()+"\t"+DS()+
 				"\n點心罐\t$60\t"+getCan()+"\t"+CS()+
-				"\n主食餐包\t$45\t"+getBag()+"\t"+BS()+
-				"\n--------------------------------------------------------------------------------------------------"+
+				"\n--------------------------------------------------------------------------------"+
 				"\n\t\t應付金額\t"+getSum();		
 	}
 	
